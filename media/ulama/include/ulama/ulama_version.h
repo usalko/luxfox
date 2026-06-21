3a14213a48c1177712c7166ed24adf826f245e0c
Vanya Usalko <dk4aol@gmail.com>
2026-06-21 21:22:29 +0300

    # There is the new ulama build number.

--- media/ulama/include/ulama/ulama_version.h
+++ media/ulama/include/ulama/ulama_version.h
@@ -1,9 +1,9 @@
 #ifndef ULAMA_VERSION_H
 #define ULAMA_VERSION_H
 
-#define ULAMA_BUILD_NUMBER    69
-#define ULAMA_GIT_HASH        "8345ba9a5"
+#define ULAMA_BUILD_NUMBER    80
+#define ULAMA_GIT_HASH        "58631cfa1"
 #define ULAMA_GIT_BRANCH      "main"
-#define ULAMA_BUILD_DATE      "2026-06-20 20:25:54"
+#define ULAMA_BUILD_DATE      "2026-06-21 20:39:54"
 
 #endif /* ULAMA_VERSION_H */
