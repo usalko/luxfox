de78bea745737a7478c909181584bdc6d85eda70
Vanya Usalko <dk4aol@gmail.com>
2026-06-26 17:45:02 +0300

    # There is the version v0.1.151

--- media/ulama/include/ulama/ulama_version.h
+++ media/ulama/include/ulama/ulama_version.h
@@ -1,9 +1,9 @@
 #ifndef ULAMA_VERSION_H
 #define ULAMA_VERSION_H
 
-#define ULAMA_BUILD_NUMBER    143
-#define ULAMA_GIT_HASH        "400db4915"
+#define ULAMA_BUILD_NUMBER    151
+#define ULAMA_GIT_HASH        "f0b781ace"
 #define ULAMA_GIT_BRANCH      "main"
-#define ULAMA_BUILD_DATE      "2026-06-26 11:01:25"
+#define ULAMA_BUILD_DATE      "2026-06-26 15:22:33"
 
 #endif /* ULAMA_VERSION_H */
