b887e37a777f1bdefaa8096d4a08770c7796a6ac
Vanya Usalko <dk4aol@gmail.com>
2026-06-27 21:49:41 +0300

    # There is the increase build number and update defaults.

--- media/ulama/include/ulama/ulama_version.h
+++ media/ulama/include/ulama/ulama_version.h
@@ -1,9 +1,9 @@
 #ifndef ULAMA_VERSION_H
 #define ULAMA_VERSION_H
 
-#define ULAMA_BUILD_NUMBER    167
-#define ULAMA_GIT_HASH        "424d89ed0"
+#define ULAMA_BUILD_NUMBER    180
+#define ULAMA_GIT_HASH        "b93387e24"
 #define ULAMA_GIT_BRANCH      "main"
-#define ULAMA_BUILD_DATE      "2026-06-27 14:14:18"
+#define ULAMA_BUILD_DATE      "2026-06-27 21:43:33"
 
 #endif /* ULAMA_VERSION_H */
