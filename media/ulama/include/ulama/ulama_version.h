a65b578bc6302bd926ee7b9980fcf49ee89d301c
Vanya Usalko <dk4aol@gmail.com>
2026-06-29 08:29:26 +0300

    # There is the renaming of the project.

--- media/ulama/include/ulama/ulama_version.h
+++ media/ulama/include/ulama/ulama_version.h
@@ -1,9 +1,9 @@
 #ifndef ULAMA_VERSION_H
 #define ULAMA_VERSION_H
 
-#define ULAMA_BUILD_NUMBER    205
-#define ULAMA_GIT_HASH        "1743f9912"
+#define ULAMA_BUILD_NUMBER    206
+#define ULAMA_GIT_HASH        "94c256cba"
 #define ULAMA_GIT_BRANCH      "main"
-#define ULAMA_BUILD_DATE      "2026-06-28 22:58:22"
+#define ULAMA_BUILD_DATE      "2026-06-29 03:56:11"
 
 #endif /* ULAMA_VERSION_H */
