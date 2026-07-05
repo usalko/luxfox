419e6dca8ca69a1adc872496a97e73443b32857c
Vanya Usalko <dk4aol@gmail.com>
2026-07-05 15:19:55 +0300

    # There us the version v0.1.4

--- media/ulama/include/ulama/ulama_version.h
+++ media/ulama/include/ulama/ulama_version.h
@@ -1,9 +1,9 @@
 #ifndef ULAMA_VERSION_H
 #define ULAMA_VERSION_H
 
-#define ULAMA_BUILD_NUMBER    409
-#define ULAMA_GIT_HASH        "b1ed1ab03"
+#define ULAMA_BUILD_NUMBER    411
+#define ULAMA_GIT_HASH        "e17dab1a3"
 #define ULAMA_GIT_BRANCH      "main"
-#define ULAMA_BUILD_DATE      "2026-07-04 23:05:18"
+#define ULAMA_BUILD_DATE      "2026-07-05 15:15:10"
 
 #endif /* ULAMA_VERSION_H */
