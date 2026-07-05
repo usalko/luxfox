86fa03bbea1f0cb0d3dace7e603a4c73ed830b07
Vanya Usalko <dk4aol@gmail.com>
2026-07-05 15:28:28 +0300

    # There is the v0.1.4 baseline: frame-atomic eviction restored, bitrate 256.

--- media/ulama/include/ulama/ulama_version.h
+++ media/ulama/include/ulama/ulama_version.h
@@ -1,9 +1,9 @@
 #ifndef ULAMA_VERSION_H
 #define ULAMA_VERSION_H
 
-#define ULAMA_BUILD_NUMBER    411
-#define ULAMA_GIT_HASH        "e17dab1a3"
+#define ULAMA_BUILD_NUMBER    412
+#define ULAMA_GIT_HASH        "419e6dca8"
 #define ULAMA_GIT_BRANCH      "main"
-#define ULAMA_BUILD_DATE      "2026-07-05 15:15:10"
+#define ULAMA_BUILD_DATE      "2026-07-05 15:20:49"
 
 #endif /* ULAMA_VERSION_H */
